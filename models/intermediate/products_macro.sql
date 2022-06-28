93506f25f80e391fe0a86cb1915ce7d2f8abc8cd
Ubuntu <ubuntu@ip-172-31-30-2.us-east-2.compute.internal>
2022-06-28 01:00:51 +0000

    created products_macro model for funzies

---
+++ models/intermediate/products_macro.sql
@@ -0,0 +1,22 @@
+with 
+
+items_ordered as (
+    select * from {{ref ('items_ordered')}}
+),
+
+final as (
+
+    select 
+        product_id,
+        product_name,
+        product_category,
+        price,
+        count(distinct order_item_id) as total_purchased,
+        sum(price) as total_revenue
+    from items_ordered
+    group by 1,2,3,4
+    order by 1,2,3,4
+
+)
+
+select * from final
