57dff1d6398bd100483178278485fd150535ae2d
Ubuntu <ubuntu@ip-172-31-30-2.us-east-2.compute.internal>
2022-06-28 01:19:10 +0000

    added a few more aggregates to revenue models

--- models/marts/weekly_revenue_by_delivery_state.sql
+++ models/marts/weekly_revenue_by_delivery_state.sql
@@ -9,7 +9,10 @@ final as (
     select
         date_trunc(created_at, week) as date_week,
         state as delivery_state,
-        sum(total) as total_revenue
+        sum(total) as total_revenue,
+        count(distinct order_id) as total_orders,
+        count(distinct order_item_id) as total_items_purchased,
+        count(distinct customer_id) as unique_customers
     from orders
     group by 1,2
     order by 1,2
